20c3e6516f3844da2670c76aa7080c3a2d188502
James Abad <james@opnetwork.gg>
2023-05-17 00:00:52 +0300

    tests

---
+++ src/test.ts
@@ -0,0 +1,22 @@
+const thing = {
+  round_status: "match_end",
+  //   round_status: "round_start",
+  //   round_status: "round_end",
+
+  players: {
+    Yames: {
+      hero: "Bastion",
+      playerName: "Yames",
+      kills: 32,
+      ultimate_status: "ended",
+      //   ultimate_status: "charged",
+      //   ultimate_status: "started",
+      deaths: 1,
+    },
+    player_2: {
+      hero: "Genji",
+      playerName: "player_2",
+      deaths: 1,
+    },
+  },
+};
