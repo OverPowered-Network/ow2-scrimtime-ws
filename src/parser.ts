3d2c98ac1c97dec50bc3a4522d4cdc390dd35547
James Abad <james@opnetwork.gg>
2023-05-17 02:57:20 +0300

    Fix stat calculations Check if directory exists before running Code cleanup

--- src/parser.ts
+++ src/parser.ts
@@ -2,15 +2,26 @@ import fs from "fs";
 import path from "path";
 import * as _ from "underscore";
 import WebSocket, { Server as WebSocketServer } from "ws";
-import { createServer, Server as httpServer } from "http";
+import { createServer } from "http";
 
 /**
  * Replace with the correct static path to the scrim-time log files directory
- * C:\Users\{username}\OneDrive\Documents\Overwatch\Workshop
+ * C:/Users/{username}/OneDrive/Documents/Overwatch/Workshop
  * or
- * C:\Users\{username}\Documents\Overwatch\Workshop
+ * C:/Users/{username}/Documents/Overwatch/Workshop
+ *
+ * Be sure to change the backslashes "\" with forward slashes "/" when copying directory path
  */
 const baseDir = "C:/Users/rjame/OneDrive/Documents/Overwatch/Workshop";
+console.log(`Directory path set to: ${baseDir}`);
+
+// Check if path exists
+const dirExsists = fs.existsSync(baseDir);
+
+if (!dirExsists) {
+  console.error("Invalid directory path!");
+  process.exit(1);
+}
 
 const OW_EVENT_KEYS = {
   RND_S: "round_start",
@@ -26,14 +37,14 @@ const OW_EVENT_KEYS = {
   M_REZ: "mercy_rez",
   K: "kill",
   M_END: "match_end",
+  M_START: "match_start",
 };
 
 // Return only base file name without dir
 const getMostRecentFileName = (dir: string) => {
   const files = fs.readdirSync(dir);
 
-  // use underscore for max()
-  return _.max(files, function (file) {
+  return _.max(files, (file) => {
     const fullpath = path.join(dir, file);
 
     // ctime = creation time is used
@@ -42,29 +53,24 @@ const getMostRecentFileName = (dir: string) => {
   });
 };
 
-let oldEvents: any[] = [];
-let events: any[] = [];
+const scrimCsvToObjArray = (str: string, delimiter = ",") => {
+  const rows = str.split("\n");
 
-function scrimCsvToArray(str: string, delimiter = ",") {
-  // slice from start of text to the first \n index
-  // use split to create an array from string by delimiter
-
-  // slice from \n index + 1 to the end of the text
-  // use split to create an array of each csv value row
-  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-
-  const key = rows[1];
-
-  // Map the rows
-  // split values from each row into an array
-  // use headers.reduce to create an object
-  // object properties derived from headers:values
-  // the object passed as an element of the array
   const arr = rows.map(function (row) {
     const values = row.split(delimiter);
-    // console.log({ values });
 
     const key = values[1];
+    if (key === OW_EVENT_KEYS.M_START) {
+      const el = {
+        [key]: {
+          mapName: values[3],
+          timestamp: values[2],
+        },
+      };
+
+      return el;
+    }
+
     if (key === OW_EVENT_KEYS.K) {
       const el = {
         [key]: {
@@ -150,7 +156,7 @@ function scrimCsvToArray(str: string, delimiter = ",") {
           },
         },
       };
-      console.log("SWAP", el);
+      // console.log("SWAP", el);
 
       return el;
     }
@@ -176,6 +182,7 @@ function scrimCsvToArray(str: string, delimiter = ",") {
     if (key === OW_EVENT_KEYS.P_STAT) {
       const el = {
         [key]: {
+          team: values[3],
           round: values[3],
           hero: values[6],
           playerName: values[5],
@@ -205,22 +212,22 @@ function scrimCsvToArray(str: string, delimiter = ",") {
 
   // return the array
   return arr;
-}
+};
 
 const createDTO = (events: any) => {
+  let MAP: string = "";
   const PLAYER_STATS = {};
   const PLAYERS = {};
   const DTO = {};
 
   for (const event of events) {
-    // console.log(event);
+    if (event[OW_EVENT_KEYS.M_START]) {
+      console.log("GOT MAP NAME!");
+      MAP = event?.[OW_EVENT_KEYS.M_START]?.mapName;
+    }
+
     if (event[OW_EVENT_KEYS.H_SPN]) {
       if (event?.[OW_EVENT_KEYS.H_SPN]?.player?.playerName) {
-        // console.log(
-        //   "CREATING PLAYER DTO",
-        //   event?.[OW_EVENT_KEYS.H_SPN]?.player?.playerName
-        // );
-
         PLAYERS[event?.[OW_EVENT_KEYS.H_SPN]?.player?.playerName] = {
           ...(PLAYERS?.[event?.[OW_EVENT_KEYS.H_SPN]?.player?.playerName] ||
             {}),
@@ -233,8 +240,6 @@ const createDTO = (events: any) => {
 
     if (event[OW_EVENT_KEYS.H_SWP]) {
       if (event?.[OW_EVENT_KEYS.H_SWP]?.player?.playerName) {
-        // console.log("SWAPPING PLAYER DTO", event?.[OW_EVENT_KEYS.H_SWP]);
-
         PLAYERS[event?.[OW_EVENT_KEYS.H_SWP]?.player?.playerName] = {
           ...PLAYERS[event?.[OW_EVENT_KEYS.H_SWP]?.player?.playerName],
           ...event[OW_EVENT_KEYS.H_SWP].player,
@@ -245,18 +250,11 @@ const createDTO = (events: any) => {
     }
 
     if (event[OW_EVENT_KEYS.K]) {
-      console.log(event[OW_EVENT_KEYS.K].recievingPlayer);
       if (event?.[OW_EVENT_KEYS.K]?.player?.playerName) {
         if (PLAYERS[event?.[OW_EVENT_KEYS.K]?.player?.playerName]) {
-          // console.log(
-          //   "ADDING KILL VALUE TO PLAYER",
-          //   event?.[OW_EVENT_KEYS.K]?.player?.playerName
-          // );
           if (PLAYERS[event?.[OW_EVENT_KEYS.K]?.player?.playerName].kills) {
-            // console.log("INCREMENTING KILL VALUE TO PLAYER");
             PLAYERS[event?.[OW_EVENT_KEYS.K]?.player?.playerName].kills += 1;
           } else {
-            // console.log("INITALIZING KILL VALUE TO PLAYER");
             PLAYERS[event?.[OW_EVENT_KEYS.K]?.player?.playerName].kills = 1;
           }
 
@@ -266,18 +264,15 @@ const createDTO = (events: any) => {
               PLAYERS[event?.[OW_EVENT_KEYS.K]?.recievingPlayer?.playerName]
                 ?.deaths
             ) {
-              // console.log("INCREMENTING DEATH VALUE TO PLAYER");
               PLAYERS[
                 event?.[OW_EVENT_KEYS.K]?.recievingPlayer?.playerName
               ].deaths += 1;
             } else {
-              // console.log("INITALIZING DEATH VALUE TO PLAYER");
               PLAYERS[
                 event?.[OW_EVENT_KEYS.K]?.recievingPlayer?.playerName
               ].deaths = 1;
             }
           } else {
-            // console.log("INITALIZING DEATH VALUE TO PLAYER");
             PLAYERS[event?.[OW_EVENT_KEYS.K]?.recievingPlayer?.playerName] = {
               ...event?.[OW_EVENT_KEYS.K]?.recievingPlayer,
               deaths: 1,
@@ -292,20 +287,14 @@ const createDTO = (events: any) => {
     if (event[OW_EVENT_KEYS.OFF_ASS]) {
       if (event?.[OW_EVENT_KEYS.OFF_ASS]?.player?.playerName) {
         if (PLAYERS[event?.[OW_EVENT_KEYS.OFF_ASS]?.player?.playerName]) {
-          // console.log(
-          //   "ADDING OFF_ASSISTS VALUE TO PLAYER",
-          //   event?.[OW_EVENT_KEYS.OFF_ASS]?.player?.playerName
-          // );
           if (
             PLAYERS[event?.[OW_EVENT_KEYS.OFF_ASS]?.player?.playerName]
               .off_assists
           ) {
-            // console.log("INCREMENTING OFF_ASSISTS VALUE TO PLAYER");
             PLAYERS[
               event?.[OW_EVENT_KEYS.OFF_ASS]?.player?.playerName
             ].off_assists += 1;
           } else {
-            // console.log("INITALIZING OFF_ASSISTS VALUE TO PLAYER");
             PLAYERS[
               event?.[OW_EVENT_KEYS.OFF_ASS]?.player?.playerName
             ].off_assists = 1;
@@ -319,20 +308,14 @@ const createDTO = (events: any) => {
     if (event[OW_EVENT_KEYS.DEF_ASS]) {
       if (event?.[OW_EVENT_KEYS.DEF_ASS]?.player?.playerName) {
         if (PLAYERS[event?.[OW_EVENT_KEYS.DEF_ASS]?.player?.playerName]) {
-          // console.log(
-          //   "ADDING DEF_ASSISTS VALUE TO PLAYER",
-          //   event?.[OW_EVENT_KEYS.DEF_ASS]?.player?.playerName
-          // );
           if (
             PLAYERS[event?.[OW_EVENT_KEYS.DEF_ASS]?.player?.playerName]
               .def_assists
           ) {
-            // console.log("INCREMENTING DEF_ASSISTS VALUE TO PLAYER");
             PLAYERS[
               event?.[OW_EVENT_KEYS.DEF_ASS]?.player?.playerName
             ].def_assists += 1;
           } else {
-            // console.log("INITALIZING DEF_ASSISTS VALUE TO PLAYER");
             PLAYERS[
               event?.[OW_EVENT_KEYS.DEF_ASS]?.player?.playerName
             ].def_assists = 1;
@@ -402,54 +385,50 @@ const createDTO = (events: any) => {
         const newHero = event?.[OW_EVENT_KEYS.P_STAT]?.hero;
         const oldHero = PLAYER_STATS[playerName]?.hero || "";
 
-        console.log(newRoundNo, oldRoundNo, newHero, oldHero);
         if (PLAYER_STATS[playerName]) {
           if (newRoundNo === oldRoundNo) {
-            if (newHero !== PLAYER_STATS[playerName].hero) {
-              console.log("Adding stats to", playerName, event);
-              PLAYER_STATS[playerName].round = newRoundNo;
-              PLAYER_STATS[playerName].hero = newHero;
-              PLAYER_STATS[playerName].heroList.push(newHero);
-              PLAYER_STATS[playerName].eliminations += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.eliminations
-              );
+            PLAYER_STATS[playerName].round = newRoundNo;
+            PLAYER_STATS[playerName].hero = newHero;
+            PLAYER_STATS[playerName].heroList.push(newHero);
+            PLAYER_STATS[playerName].eliminations += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.eliminations
+            );
 
-              PLAYER_STATS[playerName].final_blows += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.final_blows
-              );
+            PLAYER_STATS[playerName].final_blows += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.final_blows
+            );
 
-              PLAYER_STATS[playerName].deaths += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.deaths
-              );
+            PLAYER_STATS[playerName].deaths += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.deaths
+            );
 
-              PLAYER_STATS[playerName].hero_damage += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.hero_damage
-              );
+            PLAYER_STATS[playerName].hero_damage += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.hero_damage
+            );
 
-              PLAYER_STATS[playerName].healing_dealt += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.healing_dealt
-              );
+            PLAYER_STATS[playerName].healing_dealt += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.healing_dealt
+            );
 
-              PLAYER_STATS[playerName].damage_taken += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.damage_taken
-              );
+            PLAYER_STATS[playerName].damage_taken += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.damage_taken
+            );
 
-              PLAYER_STATS[playerName].damage_mitigated += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.damage_mitigated
-              );
+            PLAYER_STATS[playerName].damage_mitigated += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.damage_mitigated
+            );
 
-              PLAYER_STATS[playerName].defensive_assists += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.defensive_assists
-              );
+            PLAYER_STATS[playerName].defensive_assists += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.defensive_assists
+            );
 
-              PLAYER_STATS[playerName].offensive_assists += parseInt(
-                event?.[OW_EVENT_KEYS.P_STAT]?.offensive_assists
-              );
+            PLAYER_STATS[playerName].offensive_assists += parseInt(
+              event?.[OW_EVENT_KEYS.P_STAT]?.offensive_assists
+            );
 
-              PLAYER_STATS[playerName].weapon_accuracy += parseFloat(
-                event?.[OW_EVENT_KEYS.P_STAT]?.weapon_accuracy
-              );
-            }
+            PLAYER_STATS[playerName].weapon_accuracy += parseFloat(
+              event?.[OW_EVENT_KEYS.P_STAT]?.weapon_accuracy
+            );
           } else {
             PLAYER_STATS[playerName] = {
               round: parseInt(event?.[OW_EVENT_KEYS.P_STAT]?.round),
@@ -499,7 +478,6 @@ const createDTO = (events: any) => {
             };
           }
         } else {
-          // console.log("INITALIZING KILL VALUE TO PLAYER");
           PLAYER_STATS[playerName] = {
             round: parseInt(event?.[OW_EVENT_KEYS.P_STAT]?.round),
             hero: event?.[OW_EVENT_KEYS.P_STAT]?.hero,
@@ -549,6 +527,7 @@ const createDTO = (events: any) => {
   }
 
   DTO["player_stats"] = PLAYER_STATS;
+  DTO["map"] = MAP;
 
   return DTO;
 };
@@ -571,22 +550,11 @@ const start = () => {
         persistent: true,
         interval: 1000,
       },
-      (curr, prev) => {
+      (_curr, _prev) => {
         console.log("\nThe file was edited");
-
-        // Show the time when the file was modified
-        console.log("Previous Modified Time", prev.mtime);
-        console.log("Current Modified Time", curr.mtime);
-
         const rawStringContent = fs.readFileSync(fullPath, "utf8");
 
-        const eventArray = scrimCsvToArray(rawStringContent);
-        // console.log(eventArray);
-        events = eventArray;
-
-        // for (const event of eventArray) {
-        //   createDTO(event);
-        // }
+        const eventArray = scrimCsvToObjArray(rawStringContent);
 
         const DTO = createDTO(eventArray);
 
@@ -597,7 +565,6 @@ const start = () => {
             client.socket.send(JSON.stringify(DTO));
           }
         }
-        oldEvents = events;
       }
     );
   } else {
@@ -614,14 +581,12 @@ const WS_SERVER = new WebSocketServer({ server: HTTP_SERVER });
 WS_SERVER.on("connection", (socket, req) => {
   connectedClients.push({ key: connectedClients.length, socket });
   console.log("Socket client connected", req.headers.origin);
-  // start(socket);
 
   socket.on("disconnect", (reason) => {
     console.log(`Socket disconnected due to ${reason}`);
   });
 
   socket.on("message", (data) => {
-    console.log(data);
     if (data === "START") {
       console.log("Starting logger");
       start();
